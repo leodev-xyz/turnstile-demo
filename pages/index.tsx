01fea37f061e469f459c88d65d4c0d96a0cd971b
Leo Developer <leodeveloper@protonmail.com>
2022-10-18 20:26:34 +0200

    qoi improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Turnstile from "react-turnstile";
 
 export default function Home() {
     const [sitekey, setSitekey] = useState("1x00000000000000000000AA");
     const [action, setAction] = useState("demo");
-    const [cdata, setCData] = useState("customer data");
+    const [cdata, setCData] = useState("customerdata");
     const [theme, setTheme] = useState("auto");
+    const [size, setSize] = useState("normal");
     const [token, setToken] = useState("(loading)");
+    const [widgetId, setWidgetId] = useState("");
     return (
-        <div className="my-20 space-y-20 --container">
-            <p>
-                Sitekey:
-                <input
-                    value={sitekey}
-                    onChange={(event) => setSitekey(event.target.value)}
-                    className="ml-8 bg-gray-800 w-80"
-                />
-            </p>
+        <div className="my-20 space-y-10 --container">
+            <div>
+                <p>
+                    Sitekey:
+                    <input
+                        value={sitekey}
+                        onChange={(event) => setSitekey(event.target.value)}
+                        className="ml-8 bg-gray-800 w-80"
+                    />
+                </p>
+                {!/^[0-9A-Za-z_-]{3,100}$/i.test(sitekey) && (
+                    <p className="text-red-300">
+                        Action should match the regex:
+                        <code>
+                            {" /^[0-9A-Za-z_-]{3,100}$/i "}
+                        </code>
+                        If the widget fails to load, it's probably because of this.
+                    </p>
+                )}
+                <div className="flex gap-2 mt-2">
+                    <span>
+                        Test sitekeys:
+                    </span>
+                    <button
+                        className="--btn --btn-0 --btn-primary"
+                        onClick={() => setSitekey("1x00000000000000000000AA")}
+                    >
+                        Always passes
+                    </button>
+                    <button
+                        className="--btn --btn-0 --btn-primary"
+                        onClick={() => setSitekey("2x00000000000000000000AB")}
+                    >
+                        Always blocks
+                    </button>
+                    <button
+                        className="--btn --btn-0 --btn-primary"
+                        onClick={() => setSitekey("3x00000000000000000000FF")}
+                    >
+                        Forces an interactive challenge
+                    </button>
+                </div>
+              </div>
             <p>
                 Action:
                 <input
@@ -25,6 +61,15 @@ export default function Home() {
                     onChange={(event) => setAction(event.target.value)}
                     className="ml-8 bg-gray-800 w-80"
                 />
+                {!/^[a-z0-9_-]{0,32}$/i.test(action) && (
+                    <p className="text-red-300">
+                        Action should match the regex:
+                        <code>
+                            {" /^[a-z0-9_-]{0,32}$/i "}
+                        </code>
+                        If the widget fails to load, it's probably because of this.
+                    </p>
+                )}
             </p>
             <p>
                 cData:
@@ -33,27 +78,74 @@ export default function Home() {
                     onChange={(event) => setCData(event.target.value)}
                     className="ml-8 bg-gray-800 w-80"
                 />
+                {!/^[a-z0-9_-]{0,255}$/i.test(cdata) && (
+                    <p className="text-red-300">
+                        cData should match the regex:
+                        <code>
+                            {" /^[a-z0-9_-]{0,255}$/i "}
+                        </code>
+                        If the widget fails to load, it's probably because of this.
+                    </p>
+                )}
             </p>
-            <p>
-                Theme:
-                <input
-                    value={theme}
-                    onChange={(event) => setTheme(event.target.value)}
-                    className="ml-8 bg-gray-800 w-80"
-                />
-            </p>
+            <div>
+                <p>
+                    Theme:
+                    <input
+                        value={theme}
+                        onChange={(event) => setTheme(event.target.value)}
+                        className="ml-8 bg-gray-800 w-80"
+                    />
+                </p>
+                {!["auto", "light", "dark"].includes(theme) && (
+                    <p className="text-red-300">
+                        Theme should be one of "auto", "light" or "dark".
+                        If the widget fails to load, it's probably because of this.
+                    </p>
+                )}
+            </div>
+            <div>
+                <p>
+                    Size:
+                    <input
+                        value={size}
+                        onChange={(event) => setSize(event.target.value)}
+                        className="ml-8 bg-gray-800 w-80"
+                    />
+                </p>
+                {!["normal", "compact", "invisible"].includes(size) && (
+                    <p className="text-red-300">
+                        Size should be one of "normal", "compact" or "invisible".
+                        If the widget fails to load, it's probably because of this.
+                    </p>
+                )}
+            </div>
             <Turnstile
                 sitekey={sitekey}
                 action={action}
                 cData={cdata}
                 /* @ts-ignore */
                 theme={theme}
-                onLoad={() => setToken("(loading)")}
+                size={size}
+                onLoad={widgetId => {
+                    setToken("(loading)");
+                    setWidgetId(widgetId);
+                }}
                 onError={() => setToken("an error occured")}
                 onExpire={() => setToken("token expired")}
                 onVerify={(token) => setToken(token)}
             />
-            <p>Token: {token}</p>
+            <div>
+                <p>Token: {token}</p>
+                <p>WidgetId: {widgetId}</p>
+                <button
+                    className="--btn --btn-3 --btn-primary"
+                    onClick={() => window.turnstile.reset(widgetId)}
+                    disabled={!widgetId}
+                >
+                    Reset
+                </button>
+            </div>
         </div>
     );
 }
